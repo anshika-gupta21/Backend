eadaccdd1ca48490a8bb5aa4ea004d7f18dd1304
ANSHIKA GUPTA <anshikag.2118@gmail.com>
2025-01-29 20:54:50 +0530

    feat: add get video comments functionality; implement controller and routes for fetching comments by video ID

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,24 +1,30 @@
-//add comment
-//update comment
-//delete comment
-
 import mongoose, { isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { Comment } from "../models/comment.models.js";
 
-// const getVideoComments = asyncHandler(async (req, res) => {});
+const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!videoId || !isValidObjectId(videoId))
+    throw new ApiError(400, "Invalid video Id");
+  const comments = await Comment.find({ video: videoId });
+  if (comments.length === 0)
+    throw new ApiError(404, "No comments found for this video!");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully!"));
+});
 
 const addComment = asyncHandler(async (req, res) => {
-  const videoId = req.params;
+  const { videoId } = req.params;
   const userId = req.user?._id;
   const { content } = req.body;
 
   if (!content) throw new ApiError(400, "Content is required!");
 
-  //   if (!videoId || !isValidObjectId(videoId))
-  //     throw new ApiError(400, "Video does not exist!");
+  if (!videoId || !isValidObjectId(videoId))
+    throw new ApiError(400, "Video does not exist!");
 
   const comment = await Comment.create({
     content,
@@ -35,14 +41,14 @@ const editComment = asyncHandler(async (req, res) => {
   const { content } = req.body;
   if (!content) throw new ApiError(400, "Content is required!");
 
-  const commentId = req.params;
+  const { commentId } = req.params;
 
   if (!commentId || !isValidObjectId(commentId))
     throw new ApiError(400, "Invalid comment Id!");
 
   const comment = await Comment.findById(commentId);
   if (!comment) throw new ApiError(400, "Comment does not exist!");
-  if (req.user._id !== comment.owner)
+  if (req.user._id.toString() !== comment.owner.toString())
     throw new ApiError(402, "You cannot change this comment!");
 
   const updatedComment = await Comment.findByIdAndUpdate(
@@ -63,13 +69,16 @@ const editComment = asyncHandler(async (req, res) => {
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
-  const commentId = req.params;
-  const comment = await Comment.findById({ commentId });
-  if (!comment || !isValidObjectId(commentId))
-    throw new ApiError(400, "Comment does not exist!");
-  if (comment.owner !== req.user?._id)
+  const { commentId } = req.params;
+
+  if (!commentId || !isValidObjectId(commentId))
+    throw new ApiError(400, "Comment Id invalid!");
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new ApiError(400, "Comment does not exist!");
+  if (comment.owner.toString() !== req.user?._id.toString())
     throw new ApiError(402, "You cannot delete this comment!");
-  const isDeleted = await Comment.findByIdAndDelete({ commentId });
+  const isDeleted = await Comment.findByIdAndDelete(commentId);
   if (!isDeleted)
     throw new ApiError(500, "Something went wrong while deleting the comment!");
   return res
@@ -77,4 +86,4 @@ const deleteComment = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Comment deleted succesfully!"));
 });
 
-export { addComment, editComment, deleteComment };
+export { addComment, editComment, deleteComment, getVideoComments };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -2,13 +2,15 @@ import {
   addComment,
   deleteComment,
   editComment,
+  getVideoComments,
 } from "../controllers/comment.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { Router } from "express";
 
 const router = Router();
-router.route("/add-comment").post(verifyJWT, addComment);
-router.route("/edit-comment").patch(verifyJWT, editComment);
-router.route("/delete-comment").delete(verifyJWT, deleteComment);
+router.route("/add-comment/:videoId").post(verifyJWT, addComment);
+router.route("/edit-comment/:commentId").patch(verifyJWT, editComment);
+router.route("/delete-comment/:commentId").delete(verifyJWT, deleteComment);
+router.route("/get-comments/:videoId").get(verifyJWT, getVideoComments);
 
 export default router;
