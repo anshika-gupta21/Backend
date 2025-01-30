4c05c22641f1f922521303010be6d2cf017a91df
ANSHIKA GUPTA <anshikag.2118@gmail.com>
2025-01-30 15:11:14 +0530

    feat: add get video by ID functionality; implement controller and route for fetching video details along with comments, likes, and user subscriptions

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -156,4 +156,140 @@ const updateVideo = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, isUpdated, "Details updated successfully!"));
 });
-export { addVideo, deleteVideo, togglePublishStatus, updateVideo };
+
+const getVideoById = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  if (!videoId || !isValidObjectId(videoId))
+    throw new ApiError(400, "Invalid video id.");
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $project: {
+              content: 1,
+              owner: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "owner",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $in: [req.user?._id, "$likes.likedBy"],
+        },
+        subscriptionsCount: {
+          $size: "$subscribers",
+        },
+        isSubscribed: {
+          $in: [req.user?._id, "$subscribers.subscriber"],
+        },
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        title: 1,
+        description: 1,
+        likesCount: 1,
+        duration: 1,
+        thumbnail: 1,
+        subscriptionsCount: 1,
+        isSubscribed: 1,
+        isLiked: 1,
+        views: 1,
+        createdAt: 1,
+        owner: 1,
+        comments: 1,
+      },
+    },
+  ]);
+
+  if (!video) throw new ApiError(404, "Video not found");
+
+  const videoViews = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $inc: {
+        views: 1,
+      },
+    },
+    { new: true }
+  );
+
+  let user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $addToSet: {
+        watchHistory: videoId,
+      },
+    },
+    {
+      new: true,
+      select: "fullName username watchHistory avatar",
+    }
+  );
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { video: video[0], videoViews: videoViews.views, user: user },
+        "Video fetched successfully!"
+      )
+    );
+});
+export {
+  addVideo,
+  deleteVideo,
+  togglePublishStatus,
+  updateVideo,
+  getVideoById,
+};
--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import {
   addVideo,
   deleteVideo,
+  getVideoById,
   togglePublishStatus,
   updateVideo,
 } from "../controllers/video.controller.js";
@@ -23,6 +24,6 @@ router.route("/upload-video").post(
   ]),
   addVideo
 );
-router.route("/:videoId").delete(deleteVideo).patch(upload.single("thumbnail"), updateVideo);
+router.route("/:videoId").delete(deleteVideo).patch(upload.single("thumbnail"), updateVideo).get(getVideoById);
 router.route("/toggle-publish-status/:videoId").patch(togglePublishStatus);
 export default router;
